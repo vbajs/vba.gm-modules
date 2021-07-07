abfb9bb8f22bf9327992040b8bd4ab7e551a4f89
Yahya Wessam <yahyawessam2002@gmail.com>
2021-07-07 22:48:00 +0300

    rickrollar: v1.0.2
    Now it can inject into Youtube video embeds!
    Hopefully fix issue where settings aren't saved

--- rickrollar/index.js
+++ rickrollar/index.js
@@ -6,6 +6,7 @@ import { version } from './goosemodModule.json';
 const Message = webpackModules.find((m) => m.type && m.type.displayName === 'MessageContent');
 let Settings = {
     VIDEO_URL: 'https://cdn.discordapp.com/attachments/858899085159563265/860672912062939166/rickrolledin4k.mp4',
+    YT_EMBED_URL: 'https://www.youtube.com/embed/o-YBDTqX_ZU',
 }
 
 export default {
@@ -22,6 +23,17 @@ export default {
                         };
                     };
                 };
+
+                const embed = document.getElementsByTagName('iframe');
+                for (let i = 0; i < embed.length; i++) {
+                    const element = embed[i];
+                    // Same second reason as above and to insure it only injects to only youtube embeds
+                    if (element.getAttribute('src').startsWith('https://www.youtube.com')) {
+                        if (element.getAttribute('src') != (Settings.YT_EMBED_URL + '?autoplay=1&auto_play=1')) {
+                            element.setAttribute('src', (Settings.YT_EMBED_URL + '?autoplay=1&auto_play=1'))
+                        };
+                    };
+                };
             });
 
             createItem('RickRollar', [
@@ -29,7 +41,7 @@ export default {
 
                 {
                     type: 'header',
-                    text: 'Direct streamable URL of the video to replace all videos with (Default is 4K60fps rickroll)',
+                    text: 'Direct streamable URL of the video to replace all videos with.',
                 },
                 {
                     type: 'text-input',
@@ -39,8 +51,22 @@ export default {
                         Settings.VIDEO_URL = value
                     },
                 },
+                {
+                    type: 'header',
+                    text: 'Youtube video embed URL to replace all youtube embeds. (To get emebed link, just click share>embed and copy the yt link in the src tag)'
+                },
+                {
+                    type: 'text-input',
+                    text: 'YT EMBED URL',
+                    initialValue: () => Settings.YT_EMBED_URL,
+                    oninput: (value) => {
+                        Settings.YT_EMBED_URL = value
+                    },
+                }
             ]);
         },
+
+        getSettings: () => [Settings],
 
         onRemove: () => {
             uninject('rickastley'),
